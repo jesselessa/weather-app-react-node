5d7f82cc76f9307183c12fb9f15551c71071e6ed
jesselessa <jessica.elessa@gmail.com>
2023-12-12 05:47:58 +0100

    moved logo to  public' folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Disclosure } from "@headlessui/react";
 // Icons
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 // Logo
-import logo from "../assets/logo.png";
+import logo from "../../public/assets/logo.png";
 
 export default function Navbar() {
   // Get current location object of the page
