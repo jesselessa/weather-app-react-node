d4b0b5a518fab8394fdd34f459a564f113055f58
jesselessa <jessica.elessa@gmail.com>
2025-02-24 05:53:23 +0100

    removed disclaimer message when entering website

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,25 +8,18 @@ import Home from "./pages/Home.jsx";
 import Favorites from "./pages/Favorites.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
-import OverlayMsg from "./components/OverlayMsg.jsx";
 
 function App() {
-  const [showOverlay, setShowOverlay] = useState(true);
-
   return (
     <BrowserRouter>
-      {showOverlay && <OverlayMsg hideOverlay={() => setShowOverlay(false)} />}
-
-      {!showOverlay && (
-        <div className="min-h-screen flex flex-col justify-between">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favorites" element={<Favorites />} />
-          </Routes>
-          <Footer />
-        </div>
-      )}
+      <div className="min-h-screen flex flex-col justify-between">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+        <Footer />
+      </div>
 
       <ToastContainer autoClose={1500} draggable={false} />
     </BrowserRouter>
