0cdf55896b5763bc2c6bc6f7011828925b62b379
jesselessa <jessica.elessa@gmail.com>
2025-02-26 01:26:17 +0100

    changed API endpoint

--- src/utils/callAPI.js
+++ src/utils/callAPI.js
@@ -1,6 +1,6 @@
 export const fetchCityData = async (city) => {
-  const url = `https://weather-app-node-express-api.onrender.com/data/${city}`;
-
+  const url = `http://weather-app-nodejs-express-api-env.eba-webxbb9d.eu-north-1.elasticbeanstalk.com/data/${city}`;
+  // const url = `https://weather-app-node-express-api.onrender.com/data/${city}`;
   // const url = `http://localhost:8000/data/${city}`;
 
   try {
