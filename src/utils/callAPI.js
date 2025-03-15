cf1898e5fd8b0475cf446e23c1aad1be536be799
jesselessa <jessica.elessa@gmail.com>
2025-03-15 01:44:04 +0100

    updated Api Url

--- src/utils/callAPI.js
+++ src/utils/callAPI.js
@@ -1,5 +1,5 @@
 export const fetchCityData = async (city) => {
-  const apiUrl = `https://weather-app.jesselessa.dev/data/${city}`;
+  const apiUrl = `https://weather-app.jesselessa.dev/api/data/${city}`;
   // const apiUrl = `http://localhost:8000/data/${city}`;
 
   try {
